c203ef855d240946426a5134d64922d24b979437
renjuhu <renjuhu@msn.com>
2018-11-27 22:34:09 -0500

    * .gitignore: initial commit
    * mycorners.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * TestTab.xaml:
    * MainPage.xaml:
    * Info.plist:
    * TestTheme.xaml:
    * TestTab.xaml.cs:
    * CornerHome.xaml:
    * mycorners.csproj:
    * MainPage.xaml.cs:
    * TestTheme.xaml.cs:
    * PersonalInfo.xaml:
    * CornerHome.xaml.cs:
    * AppDelegate.cs:
    * MainTabbedPage.xaml:
    * EditPersonInfo.xaml:
    * PersonalInfo.xaml.cs:
    * EditPersonInfo.xaml.cs:
    * MainTabbedPage.xaml.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * mycorners.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * baseline_home.png:
    * mycorners.Android.csproj:
    * Default-568h@2x.png:
    * baseline_home_2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * Tabbar.axml:
    * AboutResources.txt:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * baseline_account_2x.png:
    * Toolbar.axml:
    * icon.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * Icon.png:
    * Icon.png:
    * backarrow.png:
    * Icon.png:
    * Icon.png:
    * splashscreen.png:
    * backarrow_1x.png:
    * baseline_home.png:
    * icon.xml:
    * baseline_home_2x.png:
    * splashscreen.png:
    * splashscreen.png:
    * Icon20.png:
    * Icon87.png:
    * Icon40.png:
    * Icon58.png:
    * Icon80.png:
    * Icon60.png:
    * Icon76.png:
    * Icon29.png:
    * icon_round.xml:
    * Icon120.png:
    * splashscreen.png:
    * Icon152.png:
    * baseline_account_2x.png:
    * Icon167.png:
    * Icon180.png:
    * splashscreen.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ mycorners/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace mycorners
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new EditPersonInfo();
+            MainPage = new MainTabbedPage();
+
+            //MainPage = new NavigationPage(new MainTabbedPage())
+            //{
+            //    BarBackgroundColor = Color.FromHex("#94af76"),
+            //    BarTextColor = Color.White
+            //};
+
+        }
+
+        //public async void OnBackButtonPressed(object sender, EventArgs e)
+        //{
+        //    await NavigationPage.PopAsync();
+        //}
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ mycorners/PersonalInfo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace mycorners
+{
+    public partial class PersonalInfo : ContentPage
+    {
+        public PersonalInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
